e9cb4190e7ef03c2b353e43a980e51f530e0c464
Lodder <lodder15@hotmail.co.uk>
2017-10-29 14:29:48 +0000

    Update naming conventions

--- detect.php
+++ detect.php
@@ -1,105 +1,105 @@
 <?php
 
-class JoomlaVersions
+class JoomlaVersion
 {
-    public function getJoomlaVersion($site)
-    {
-        // Add http prefix if missing
-        if (strpos($site,'http://') === false)
-        {
-            $site = 'http://'.$site;
-        }
+	public function get($site)
+	{
+		// Add http prefix if missing
+		if (strpos($site, 'http://') === false)
+		{
+			$site = 'http://' . $site;
+		}
 
-        // Get the number value from the <version> tag in the XML file
-        $dom = new DOMDocument;
-        $url = $site . '/administrator/manifests/files/joomla.xml';
-        libxml_use_internal_errors(true);
+		// Get the number value from the <version> tag in the XML file
+		$dom = new DOMDocument;
+		$url = $site . '/administrator/manifests/files/joomla.xml';
+		libxml_use_internal_errors(true);
 
-        $exists = $this->XMLexists($url);
+		$exists = $this->xmlExists($url);
 
-        if ($exists)
-        {
-            $dom->load($url);
-            $versions = $dom->getElementsByTagName('version');
+		if ($exists)
+		{
+			$dom->load($url);
+			$versions = $dom->getElementsByTagName('version');
 
-            foreach ($versions as $version)
-            {
-                return $version->nodeValue;
-            }
-        }
-        else
-        {
-            $mce = $this->getTinyMCEversion($site);
+			foreach ($versions as $version)
+			{
+				return $version->nodeValue;
+			}
+		}
+		else
+		{
+			$mce = $this->getTinyVersion($site);
 
-            if ($mce)
-            {
-                // Base Joomla version on the TinyMCE version
-                switch ($mce)
-                {
-                    case '3.5.6':
-                        $joomla = '3.0.0 - 3.1.6';
-                        break;
-                    case '4.0.10':
-                        $joomla = '3.2.0 - 3.2.1';
-                        break;
-                    case '4.0.12':
-                        $joomla = '3.2.2';
-                        break;
-                    case '4.0.18':
-                        $joomla = '3.2.3 - 3.2.4';
-                        break;
-                    case '4.0.22':
-                        $joomla = '3.3.0';
-                        break;
-                    case '4.0.28':
-                        $joomla = '3.3.1 - 3.3.6';
-                        break;
-                    case '4.1.7':
-                        $joomla = '3.4.0 - 3.4.1';
-                        break;
-                    default:
-                        $joomla = '3.x';
-                }
+			if ($mce)
+			{
+				// Base Joomla version on the TinyMCE version
+				switch ($mce)
+				{
+					case '3.5.6':
+						$joomla = '3.0.0 - 3.1.6';
+						break;
+					case '4.0.10':
+						$joomla = '3.2.0 - 3.2.1';
+						break;
+					case '4.0.12':
+						$joomla = '3.2.2';
+						break;
+					case '4.0.18':
+						$joomla = '3.2.3 - 3.2.4';
+						break;
+					case '4.0.22':
+						$joomla = '3.3.0';
+						break;
+					case '4.0.28':
+						$joomla = '3.3.1 - 3.3.6';
+						break;
+					case '4.1.7':
+						$joomla = '3.4.0 - 3.4.1';
+						break;
+					default:
+						$joomla = '3.x';
+				}
 
-                return $joomla;
-            }
-            else
-            {
-                return 'Unknown';
-            }
-        }
-    }
+				return $joomla;
+			}
+			else
+			{
+				return 'Unknown';
+			}
+		}
+	}
 
-    // Get TinyMCE Version 
-    private function getTinyMCEversion($site)
-    {
-        $tinymce = $site . '/plugins/editors/tinymce/tinymce.xml';
-        libxml_use_internal_errors(true);
+	// Get TinyMCE Version 
+	private function getTinyVersion($site)
+	{
+		$tinymce = $site . '/plugins/editors/tinymce/tinymce.xml';
+		libxml_use_internal_errors(true);
 
-        $exists = $this->XMLexists($tinymce);
+		$exists = $this->xmlExists($tinymce);
 
-        if ($exists)
-        {
-            $dom->load($tinymce);
-            $vTag = $dom->getElementsByTagName('version');
+		if ($exists)
+		{
+			$dom->load($tinymce);
+			$vTag = $dom->getElementsByTagName('version');
 
-            foreach ($vTag as $tag)
-            {
-                return $tag->nodeValue;
-            }
-        }
-    }
+			foreach ($vTag as $tag)
+			{
+				return $tag->nodeValue;
+			}
+		}
+	}
 
-    // Check file exists using CURL
-    private function XMLexists($url)
-    {
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_NOBODY, true);
-        curl_exec($ch);
-        $getinfo = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
+	// Check file exists using CURL
+	private function xmlExists($url)
+	{
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_NOBODY, true);
+		curl_exec($ch);
+		$getinfo = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
 
-        return $getinfo;
-    }
-
+		return $getinfo;
+	}
 }
+
